ae803b472f16fa9632baba6bb7eb14d6f8bfabeb
steviesama <steviesama@gmail.com>
2014-08-09 14:39:20 -0500

    sync

--- SimPatient/Viewers/MarArchiveViewer.xaml.cs
+++ SimPatient/Viewers/MarArchiveViewer.xaml.cs
@@ -21,7 +21,18 @@ namespace SimPatient
 		{
 			this.InitializeComponent();
 			Owner = MainWindow.Instance;
+			
 			marViewer.VisualState = "NoInputVisualState";
+
+			adminDatePicker.SelectedDate = DateTime.Today;
+			simulationComboBox.DataContext = Simulation.Simulations;
+			Simulation.refreshSimulations();
+
+			//use async invocation to allow the ui thread to update the combo box from the previous actions
+			Dispatcher.BeginInvoke(new Action(() => {
+				if (simulationComboBox.Items.Count > 0)
+					simulationComboBox.SelectedIndex = 0;
+			}));
 		}
-	}
-}
+	} //End class MarArchiveViewer
+} //End namespace SimPatient
